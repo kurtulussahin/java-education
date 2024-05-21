d7d771c5c8402671d2c12ee002c81914e01e4cca
kurtulus <kurtulus@acer>
2024-05-21 23:17:35 +0300

    comments example added

---
+++ com.kurtulussahin.java/src/com/kurtulussahin/java/comments/Comments.java
@@ -0,0 +1,29 @@
+package com.kurtulussahin.java.comments;
+/**
+ * @author <a href="mailto:kurtulussahin@gmail.com">Kurtuluş Şahin</a>
+ *         <p>
+ *         For more info please
+ * @see <a href="http://www.kurtulussahin.com">http://www.kurtulussahin.com</a>
+ *      </p>
+ *      That's a <b>Javadoc</b> comment!
+ *                                    
+ */
+public class Comments {
+
+	private String name = "Comments"; // That's just a String.
+
+	/**
+	 * This method calculates the double of passed parameter and returns it.
+	 * 
+	 * @param x		The value to be doubled.
+	 * @return 		Double of the value of x passed as parameter.
+	 */
+	public int doubleIt(int x) {
+
+		/*
+		 * That's a comment block!
+		 */
+		return 2 * x; // Just multiply x by 2!
+	}
+}
+
