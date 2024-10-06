75a370f33b226d59e1131bc85ff703a74cf1bc8b
kurtulussahin <kurtulussahin@gmail.com>
2024-10-06 19:46:43 +0300

    item6 refactored, boxing unboxing examples added

---
+++ src/com/kurtulussahin/java/JoshuaBlochEfectiveJavaTutorial/effectivejava/item6/BoxingUnboxingTest.java
@@ -0,0 +1,11 @@
+package com.kurtulussahin.java.JoshuaBlochEfectiveJavaTutorial.effectivejava.item6;
+
+import org.junit.jupiter.api.Test;
+
+class BoxingUnboxingTest {
+
+    @Test
+    void printTimeOfLongFunctions() {
+        BoxingUnboxing.printTimeOfLongFunctions();
+    }
+}
