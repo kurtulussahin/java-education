917a044e8322fd8545f2edc625787d9a4cdd19b9
kurtulussahin <kurtulussahin@gmail.com>
2024-10-10 20:31:29 +0300

    defensive tests added

---
+++ src/com/kurtulussahin/java/JoshuaBlochEfectiveJavaTutorial/effectivejava/itema50/DefansiveDateTest.java
@@ -0,0 +1,21 @@
+package com.kurtulussahin.java.JoshuaBlochEfectiveJavaTutorial.effectivejava.itema50;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DefansiveDateTest {
+
+    @Test
+    void testDefansiveDateCopy() {
+        Date date1=new Date();
+        Date date2=date1;
+        Date date3=new Date(date1.getTime());
+
+        assertTrue(date1==date2);
+        assertFalse(date1==date3);
+        assertTrue(date1.equals(date3));
+    }
+}
