9c5923dce7a455b996570b66922444483a840fc6
kurtulussahin <kurtulussahin@gmail.com>
2024-10-17 22:52:58 +0300

    ordinary numbers

---
+++ src/com/kurtulussahin/java/codekata/emilybache/fizzbuzztdd/FizzBuzz.java
@@ -0,0 +1,8 @@
+package com.kurtulussahin.java.codekata.emilybache.fizzbuzztdd;
+
+public class FizzBuzz {
+
+    public String convert(int i){
+        return "" +i;
+    }
+}
---
+++ src/com/kurtulussahin/java/codekata/emilybache/fizzbuzztdd/FizzBuzzTest.java
@@ -0,0 +1,28 @@
+package com.kurtulussahin.java.codekata.emilybache.fizzbuzztdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/*
+
+1->1
+2->2
+3->Fizz
+4->4
+5->Buzz
+...
+15->FizzBuzz
+...
+
+*/
+
+public class FizzBuzzTest {
+
+
+    @Test
+    void ordinaryNumbersArePreserved(){
+        assertEquals("1", new FizzBuzz().convert(1));
+        assertEquals("2", new FizzBuzz().convert(2));
+    }
+}
