8145b0cdcd0fe712d55687b0a8d3bdca9d0c7122
kurtulussahin <kurtulussahin@gmail.com>
2024-10-18 23:26:27 +0300

    DoublyLinkedListNode implemented

---
+++ src/com/kurtulussahin/java/codekata/dataStructureImplementations/doublyLinkedListTdd/DoublyLinkedListTest.java
@@ -0,0 +1,20 @@
+package com.kurtulussahin.java.codekata.dataStructureImplementations.doublyLinkedListTdd;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+
+public class DoublyLinkedListTest {
+
+    @Test
+    void createDoublyLinkedListNode(){
+
+        DoublyLinkedListNode node = new DoublyLinkedListNode(10);
+        assertNull(node.next);
+        assertNull(node.prev);
+        assertEquals(node.val,10);
+    }
+}
