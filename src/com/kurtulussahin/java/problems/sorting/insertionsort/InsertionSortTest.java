dcfb4f98765d25c62da94410fa03b6481d62221c
kurtulussahin <kurtulussahin@gmail.com>
2024-10-19 22:15:15 +0300

    insertion sort fully implemented

--- src/com/kurtulussahin/java/problems/sorting/insertionsort/InsertionSortTest.java
+++ src/com/kurtulussahin/java/problems/sorting/insertionsort/InsertionSortTest.java
@@ -27,4 +27,11 @@ public class InsertionSortTest {
         int[] sorted = InsertionSort.insertionSort(unsorted);
         assertArrayEquals(new int[]{1,2,3}, sorted);
     }
+
+    @Test
+    void sortAnArray(){
+        int[] unsorted = {2,3,1,5,7,4,9,0,100,2};
+        int[] sorted = InsertionSort.insertionSort(unsorted);
+        assertArrayEquals(new int[]{0,1,2,2,3,4,5,7,9,100}, sorted);
+    }
 }
