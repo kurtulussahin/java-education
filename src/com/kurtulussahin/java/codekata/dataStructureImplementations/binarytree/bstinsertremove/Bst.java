8a29958f70ed8c39bc31507a0d762bd809ecfa16
kurtulussahin <kurtulussahin@gmail.com>
2024-10-21 19:47:23 +0300

    Bst insert implemented

--- src/com/kurtulussahin/java/codekata/dataStructureImplementations/binarytree/bstinsertremove/Bst.java
+++ src/com/kurtulussahin/java/codekata/dataStructureImplementations/binarytree/bstinsertremove/Bst.java
@@ -6,4 +6,19 @@ public class Bst {
     public Bst(int val) {
         root=new TreeNode(val);
     }
+
+    public TreeNode insert(TreeNode root, int val) {
+
+        if(root==null){
+            return new TreeNode(val);
+        }
+
+        if(val<root.val){
+            root.left = insert(root.left, val);
+        }else if (val>root.val) {
+            root.right = insert(root.right, val);
+        }
+
+        return root;
+    }
 }
--- src/com/kurtulussahin/java/codekata/dataStructureImplementations/binarytree/bstinsertremove/BstInsertAndRemoveTest.java
+++ src/com/kurtulussahin/java/codekata/dataStructureImplementations/binarytree/bstinsertremove/BstInsertAndRemoveTest.java
@@ -23,4 +23,27 @@ public class BstInsertAndRemoveTest {
         Bst tree = new Bst(10);
 
     }
+
+    @Test
+    void insertRightLeaf(){
+        Bst tree = new Bst(10);
+        tree.insert(tree.root, 11);
+        assertEquals(11, tree.root.right.val);
+    }
+
+    @Test
+    void insertLeftLeaf(){
+        Bst tree = new Bst(10);
+        tree.insert(tree.root,9);
+        assertEquals(9, tree.root.left.val);
+    }
+
+    @Test
+    void insertTwoLeftLeaf(){
+        Bst tree = new Bst(10);
+        tree.insert(tree.root,9);
+        tree.insert(tree.root,8);
+        assertEquals(9, tree.root.left.val);
+        assertEquals(8, tree.root.left.left.val);
+    }
 }
