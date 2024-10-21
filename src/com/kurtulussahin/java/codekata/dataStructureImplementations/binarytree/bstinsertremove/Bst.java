8f9a30ac74d5985ffd0d92efcde40bd04f48d869
kurtulussahin <kurtulussahin@gmail.com>
2024-10-21 20:13:05 +0300

    Bst remove implemented

--- src/com/kurtulussahin/java/codekata/dataStructureImplementations/binarytree/bstinsertremove/Bst.java
+++ src/com/kurtulussahin/java/codekata/dataStructureImplementations/binarytree/bstinsertremove/Bst.java
@@ -21,4 +21,39 @@ public class Bst {
 
         return root;
     }
+
+    public TreeNode minValueNode(TreeNode root) {
+        TreeNode curr=root;
+
+        while (curr != null && curr.left!=null){
+            curr=curr.left;
+        }
+
+        return curr;
+    }
+
+    public TreeNode remove(TreeNode root, int val) {
+
+        if(root==null){
+            return null;
+        }
+
+        if(val>root.val){
+            root.right=remove(root.right, val);
+        }else if (val<root.val){
+            root.left=remove(root.left, val);;
+        }else {
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }else {
+                TreeNode minNode = minValueNode(root.right);
+                root.val = minNode.val;
+                root.right = remove(root.right, minNode.val);
+            }
+        }
+
+        return root;
+    }
 }
--- src/com/kurtulussahin/java/codekata/dataStructureImplementations/binarytree/bstinsertremove/BstInsertAndRemoveTest.java
+++ src/com/kurtulussahin/java/codekata/dataStructureImplementations/binarytree/bstinsertremove/BstInsertAndRemoveTest.java
@@ -46,4 +46,61 @@ public class BstInsertAndRemoveTest {
         assertEquals(9, tree.root.left.val);
         assertEquals(8, tree.root.left.left.val);
     }
+
+    @Test
+    void getMinVAlueNode(){
+        Bst tree = new Bst(10);
+        tree.insert(tree.root,9);
+        tree.insert(tree.root,5);
+        tree.insert(tree.root,7);
+        tree.insert(tree.root,3);
+        tree.insert(tree.root,20);
+        tree.insert(tree.root,30);
+        tree.insert(tree.root,21);
+        assertEquals(3, tree.minValueNode(tree.root).val);
+    }
+
+    @Test
+    void removeRightLeaf(){
+        Bst tree = new Bst(10);
+        tree.insert(tree.root, 11);
+        tree.remove(tree.root, 11);
+        assertNull(tree.root.right);
+    }
+
+    @Test
+    void removeLeftLeaf(){
+        Bst tree = new Bst(10);
+        tree.insert(tree.root, 9);
+        tree.remove(tree.root, 9);
+        assertNull(tree.root.left);
+    }
+
+    @Test
+    void removeTwoRightLeaf(){
+        Bst tree = new Bst(10);
+        tree.insert(tree.root, 11);
+        tree.insert(tree.root, 12);
+
+        tree.remove(tree.root, 12);
+        assertNull(tree.root.right.right);
+    }
+
+    @Test
+    void removeInBetweenNode(){
+        Bst tree = new Bst(10);
+        tree.insert(tree.root,9);
+        tree.insert(tree.root,5);
+        tree.insert(tree.root,7);
+        tree.insert(tree.root,3);
+        tree.insert(tree.root,20);
+        tree.insert(tree.root,30);
+        tree.insert(tree.root,21);
+
+        tree.remove(tree.root, 9);
+        assertEquals(10, tree.root.val);
+        assertEquals(5, tree.root.left.val);
+        assertEquals(7, tree.root.left.right.val);
+
+    }
 }
