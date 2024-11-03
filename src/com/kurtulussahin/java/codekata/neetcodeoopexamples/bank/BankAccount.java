5218089f8b96bc2318e858321ba4fe0b8ce0da67
kurtulussahin <kurtulussahin@gmail.com>
2024-11-03 20:00:06 +0300

    getBalance implemented

--- src/com/kurtulussahin/java/codekata/neetcodeoopexamples/bank/BankAccount.java
+++ src/com/kurtulussahin/java/codekata/neetcodeoopexamples/bank/BankAccount.java
@@ -10,4 +10,8 @@ public class BankAccount {
         this.name = name;
         this.balance = balance;
     }
+
+    public int getBalance() {
+        return balance;
+    }
 }
--- src/com/kurtulussahin/java/codekata/neetcodeoopexamples/bank/BankTest.java
+++ src/com/kurtulussahin/java/codekata/neetcodeoopexamples/bank/BankTest.java
@@ -2,10 +2,21 @@ package com.kurtulussahin.java.codekata.neetcodeoopexamples.bank;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BankTest {
 
     @Test
     void createBankAccount(){
-        BankAccount account = new BankAccount (1, "Kurtuluş Şahin", 9999);
+        BankAccount account = new BankAccount (1, "Kurtuluş Şahin", 999);
     }
+
+    @Test
+    void getBalance(){
+        BankAccount account = new BankAccount (1, "Kurtuluş Şahin", 999);
+        assertEquals(999,account.getBalance() );
+    }
+
+
+
 }
