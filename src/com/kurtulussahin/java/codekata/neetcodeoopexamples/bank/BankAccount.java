40f85c7e00981cda3cd6b6dd6f6c9b622fc021cc
kurtulussahin <kurtulussahin@gmail.com>
2024-11-03 16:03:30 +0300

    BankAccount created

---
+++ src/com/kurtulussahin/java/codekata/neetcodeoopexamples/bank/BankAccount.java
@@ -0,0 +1,13 @@
+package com.kurtulussahin.java.codekata.neetcodeoopexamples.bank;
+
+public class BankAccount {
+    private int customerId;
+    private String name;
+    private int balance;
+
+    public BankAccount(int customerId, String name, int balance) {
+        this.customerId = customerId;
+        this.name = name;
+        this.balance = balance;
+    }
+}
--- src/com/kurtulussahin/java/codekata/neetcodeoopexamples/bank/BankTest.java
+++ src/com/kurtulussahin/java/codekata/neetcodeoopexamples/bank/BankTest.java
@@ -4,4 +4,8 @@ import org.junit.jupiter.api.Test;
 
 public class BankTest {
 
+    @Test
+    void createBankAccount(){
+        BankAccount account = new BankAccount (1, "Kurtuluş Şahin", 9999);
+    }
 }
