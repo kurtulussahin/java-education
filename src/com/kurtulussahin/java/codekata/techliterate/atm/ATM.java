414d14ed92d46a1602acbb6e5ab4b982e7039c41
kurtulussahin <kurtulussahin@gmail.com>
2024-11-07 21:26:49 +0300

    test cases in main created

--- src/com/kurtulussahin/java/codekata/techliterate/atm/ATM.java
+++ src/com/kurtulussahin/java/codekata/techliterate/atm/ATM.java
@@ -1,7 +1,17 @@
 package com.kurtulussahin.java.codekata.techliterate.atm;
 
+import java.util.Scanner;
+
 public class ATM {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Bank bank = new Bank("JavaBank");
+
+        User user = bank.addUser("John", "Doe", "1234");
+
+        Account account = new Account("Checking", user,bank);
+        user.addAccount(account);
+        bank.addAccount(account);
 
     }
 }
