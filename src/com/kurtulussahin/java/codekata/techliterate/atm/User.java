46dc868bdaa2c4098d0959ab54b95c21d10d221d
kurtulussahin <kurtulussahin@gmail.com>
2024-11-07 19:45:54 +0300

    user class fields created

--- src/com/kurtulussahin/java/codekata/techliterate/atm/User.java
+++ src/com/kurtulussahin/java/codekata/techliterate/atm/User.java
@@ -1,4 +1,12 @@
 package com.kurtulussahin.java.codekata.techliterate.atm;
 
+import java.util.ArrayList;
+
 public class User {
+
+    private String firstName;
+    private String lastname;
+    private String uuid;
+    private byte pinHash[];
+    private ArrayList<Account> accounts;
 }
