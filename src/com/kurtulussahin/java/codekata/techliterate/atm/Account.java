759da00aaa9387d15d24e8049734e675ce77167e
kurtulussahin <kurtulussahin@gmail.com>
2024-11-07 19:47:58 +0300

    Account class fields created

--- src/com/kurtulussahin/java/codekata/techliterate/atm/Account.java
+++ src/com/kurtulussahin/java/codekata/techliterate/atm/Account.java
@@ -1,4 +1,13 @@
 package com.kurtulussahin.java.codekata.techliterate.atm;
 
+import java.util.ArrayList;
+
 public class Account {
+
+    private String name;
+    private double balance;
+    private String uuid;
+    private User holder;
+    private ArrayList<Transaction> transactions;
+
 }
