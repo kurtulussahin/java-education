8827b46073f846ca4d4f8b161fabc97e7134eca4
kurtulussahin <kurtulussahin@gmail.com>
2024-11-07 20:17:18 +0300

    balance field removed from Account class for the sake of simplicity

--- src/com/kurtulussahin/java/codekata/techliterate/atm/Account.java
+++ src/com/kurtulussahin/java/codekata/techliterate/atm/Account.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 public class Account {
 
     private String name;
-    private double balance;
     private String uuid;
     private User holder;
     private ArrayList<Transaction> transactions;
