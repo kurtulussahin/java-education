905c2a220cbcf2bf9e65081b141676b55ff90cf8
kurtulussahin <kurtulussahin@gmail.com>
2024-11-07 19:50:01 +0300

    Transaction class fields created

--- src/com/kurtulussahin/java/codekata/techliterate/atm/Transaction.java
+++ src/com/kurtulussahin/java/codekata/techliterate/atm/Transaction.java
@@ -1,4 +1,11 @@
 package com.kurtulussahin.java.codekata.techliterate.atm;
 
+import java.util.Date;
+
 public class Transaction {
+
+    private double amount;
+    private Date timestamp;
+    private String memo;
+    private Account inAccount;
 }
