69753427b01b63578722356eab91fd2219a22dc7
kurtulussahin <kurtulussahin@gmail.com>
2024-11-07 19:55:41 +0300

    Bank class fields created

--- src/com/kurtulussahin/java/codekata/techliterate/atm/Bank.java
+++ src/com/kurtulussahin/java/codekata/techliterate/atm/Bank.java
@@ -1,4 +1,10 @@
 package com.kurtulussahin.java.codekata.techliterate.atm;
 
+import java.util.ArrayList;
+
 public class Bank {
+
+    private String name;
+    private ArrayList<User> user;
+    private ArrayList<Account> accounts;
 }
