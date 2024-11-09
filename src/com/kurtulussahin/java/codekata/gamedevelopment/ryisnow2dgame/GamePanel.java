ee255a03ff9e68a544331a7fdc05da5cf14ec55f
kurtulussahin <kurtulussahin@gmail.com>
2024-11-09 14:45:47 +0300

    graphic rectangle on window created

--- src/com/kurtulussahin/java/codekata/gamedevelopment/ryisnow2dgame/GamePanel.java
+++ src/com/kurtulussahin/java/codekata/gamedevelopment/ryisnow2dgame/GamePanel.java
@@ -29,8 +29,6 @@ public class GamePanel extends JPanel implements Runnable {
     public void run() {
 
         while(gameThread != null){
-            System.out.println("The game loop is running");
-
             update();
             repaint(); //paintComponent()
         }
@@ -44,6 +42,12 @@ public class GamePanel extends JPanel implements Runnable {
     public void paintComponent(Graphics g){
 
         super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        g2.setColor(Color.white);
+        g2.fillRect(100,100,tileSize, tileSize);
+        g2.dispose();
 
     }
 }
