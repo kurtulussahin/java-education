134d338305adbb46ae09e620b7f8d24bc59e09a2
kurtulussahin <kurtulussahin@gmail.com>
2024-11-09 11:43:48 +0300

    thread (time for game) created

--- src/com/kurtulussahin/java/codekata/gamedevelopment/ryisnow2dgame/GamePanel.java
+++ src/com/kurtulussahin/java/codekata/gamedevelopment/ryisnow2dgame/GamePanel.java
@@ -3,7 +3,7 @@ package com.kurtulussahin.java.codekata.gamedevelopment.ryisnow2dgame;
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
     final int originalTileSize = 16; //16*16 tile
     final int scale=3;
 
@@ -13,9 +13,37 @@ public class GamePanel extends JPanel {
     final int screenWith = tileSize * maxScreenCol; //768 pixels
     final int screenHeight = tileSize * maxScreenRow; //576 pixels
 
+    Thread gameThread;
+
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWith, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread(){
+        gameThread = new Thread(this);
+        gameThread.start(); //starts run method
+    }
+    @Override
+    public void run() {
+
+        while(gameThread != null){
+            System.out.println("The game loop is running");
+
+            update();
+            repaint(); //paintComponent()
+        }
+
+    }
+
+    public void update(){
+
+    }
+
+    public void paintComponent(Graphics g){
+
+        super.paintComponent(g);
+
     }
 }
