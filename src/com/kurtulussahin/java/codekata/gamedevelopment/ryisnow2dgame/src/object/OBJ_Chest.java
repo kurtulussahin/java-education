4557f8ea61638770fd63e29e8bb447b5cffb1c29
kurtulussahin <kurtulussahin@gmail.com>
2024-11-11 19:34:55 +0300

    chest and door objects placed complated

---
+++ src/com/kurtulussahin/java/codekata/gamedevelopment/ryisnow2dgame/src/object/OBJ_Chest.java
@@ -0,0 +1,16 @@
+package com.kurtulussahin.java.codekata.gamedevelopment.ryisnow2dgame.src.object;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+
+public class OBJ_Chest extends SuperObject {
+    public OBJ_Chest(){
+        name="chest";
+        try{
+            image = ImageIO.read(new File(
+                    "src/com/kurtulussahin/java/codekata/gamedevelopment/ryisnow2dgame/res/objects/chest.png"));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
