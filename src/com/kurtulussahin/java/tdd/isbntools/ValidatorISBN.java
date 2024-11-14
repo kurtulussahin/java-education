eb5ac479860c0a4d8a10c6e8baeeb01f1e4cd194
kurtulussahin <kurtulussahin@gmail.com>
2024-11-14 23:12:57 +0300

    test environment prepared

---
+++ src/com/kurtulussahin/java/tdd/isbntools/ValidatorISBN.java
@@ -0,0 +1,4 @@
+package com.kurtulussahin.java.tdd.isbntools;
+
+public class ValidatorISBN {
+}
---
+++ src/com/kurtulussahin/java/tdd/isbntools/test/ExampleTests.java
@@ -0,0 +1,11 @@
+package com.kurtulussahin.java.tdd.isbntools.test;
+import org.junit.Test;
+import static org.junit.Assert.fail;
+
+public class ExampleTests {
+
+    @Test
+    public void exampleTest(){
+        //fail();
+    }
+}
