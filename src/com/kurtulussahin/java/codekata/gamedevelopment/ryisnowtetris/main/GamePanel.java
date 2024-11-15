0390f18fdbc97ec2d3f9de1f17f2151a10437bde
kurtulussahin <kurtulussahin@gmail.com>
2024-11-15 21:42:14 +0300

    gamePanel setted to window

---
+++ src/com/kurtulussahin/java/codekata/gamedevelopment/ryisnowtetris/main/GamePanel.java
@@ -0,0 +1,16 @@
+package com.kurtulussahin.java.codekata.gamedevelopment.ryisnowtetris.main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+
+    public static final int WIDTH=1280;
+    public static final int HEIGHT=720;
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        this.setBackground(Color.black);
+        this.setLayout((null));
+    }
+}
--- src/com/kurtulussahin/java/codekata/gamedevelopment/ryisnowtetris/main/Main.java
+++ src/com/kurtulussahin/java/codekata/gamedevelopment/ryisnowtetris/main/Main.java
@@ -10,6 +10,9 @@ public class Main {
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
 
+        GamePanel gp = new GamePanel();
+        window.add(gp);
+        window.pack();
         window.setLocationRelativeTo(null);
         window.setVisible(true);
     }
