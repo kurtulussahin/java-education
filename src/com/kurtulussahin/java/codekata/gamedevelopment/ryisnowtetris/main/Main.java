bb70fde0b5e49f7ce7be73a8abb69adb0786cfd8
kurtulussahin <kurtulussahin@gmail.com>
2024-11-15 21:35:52 +0300

    RyiSnow Simple Tetris first commit

---
+++ src/com/kurtulussahin/java/codekata/gamedevelopment/ryisnowtetris/main/Main.java
@@ -0,0 +1,16 @@
+package com.kurtulussahin.java.codekata.gamedevelopment.ryisnowtetris.main;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        JFrame window = new JFrame(("Simple tetris"));
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
