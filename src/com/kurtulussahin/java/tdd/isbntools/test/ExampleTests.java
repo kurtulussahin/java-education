9f8f368e35c3ffd415d050de4150b78d5bf885b1
kurtulussahin <kurtulussahin@gmail.com>
2024-11-15 19:36:42 +0300

    ten digit loop implemented

--- src/com/kurtulussahin/java/tdd/isbntools/test/ExampleTests.java
+++ src/com/kurtulussahin/java/tdd/isbntools/test/ExampleTests.java
@@ -10,14 +10,17 @@ public class ExampleTests {
     @Test
     public void checkValidISBN(){
         ValidatorISBN validator = new ValidatorISBN();
-        boolean result = validator.checkISBN(140449116);
-        assertTrue(result);
+        boolean result = validator.checkISBN("0140449116");
+        assertTrue("First value" ,result);
+        result = validator.checkISBN("0140177396");
+        assertTrue("Second value", result);
     }
 
     @Test
     public void checkInValidISBN(){
         ValidatorISBN validator = new ValidatorISBN();
-        boolean result = validator.checkISBN(140449117);
+        boolean result = validator.checkISBN("0140449117");
         assertFalse(result);
     }
+
 }
