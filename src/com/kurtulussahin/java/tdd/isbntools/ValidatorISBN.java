bded3da02cfb639ea242d294fd576cf36669719f
kurtulussahin <kurtulussahin@gmail.com>
2024-11-15 19:08:10 +0300

    nonfunctional first test created

--- src/com/kurtulussahin/java/tdd/isbntools/ValidatorISBN.java
+++ src/com/kurtulussahin/java/tdd/isbntools/ValidatorISBN.java
@@ -1,4 +1,11 @@
 package com.kurtulussahin.java.tdd.isbntools;
 
 public class ValidatorISBN {
+    public boolean checkISBN(int isbn) {
+        if(isbn==140449116) {
+            return true;
+        }
+
+        return false;
+    }
 }
--- src/com/kurtulussahin/java/tdd/isbntools/test/ExampleTests.java
+++ src/com/kurtulussahin/java/tdd/isbntools/test/ExampleTests.java
@@ -1,11 +1,23 @@
 package com.kurtulussahin.java.tdd.isbntools.test;
+import com.kurtulussahin.java.tdd.isbntools.ValidatorISBN;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 import static org.junit.Assert.fail;
 
 public class ExampleTests {
 
     @Test
-    public void exampleTest(){
-        //fail();
+    public void checkValidISBN(){
+        ValidatorISBN validator = new ValidatorISBN();
+        boolean result = validator.checkISBN(140449116);
+        assertTrue(result);
+    }
+
+    @Test
+    public void checkInValidISBN(){
+        ValidatorISBN validator = new ValidatorISBN();
+        boolean result = validator.checkISBN(140449117);
+        assertFalse(result);
     }
 }
