beb81d1af71fec1af00d0404eb539ad22a93a4a8
kurtulussahin <kurtulussahin@gmail.com>
2024-11-17 15:57:24 +0300

    tautology example added

---
+++ src/com/kurtulussahin/java/tdd/isbntools/tautologies/NumberValidator.java
@@ -0,0 +1,13 @@
+package com.kurtulussahin.java.tdd.isbntools.tautologies;
+
+public class NumberValidator {
+
+	public boolean isItPrime(int number) {
+		int maxDivisor = (int)Math.sqrt(number); 
+		for(int i=2;i<=maxDivisor;i++) {
+	        if(number % i==0)
+	            return false;
+	    }
+	    return true;
+	}
+}
