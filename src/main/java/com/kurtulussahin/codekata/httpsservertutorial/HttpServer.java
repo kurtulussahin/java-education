9ac40f02d84f308066171a6a07e233e682bc9a42
kurtulussahin <kurtulussahin@gmail.com>
2024-11-22 23:56:43 +0300

    Confriguration manager tested

--- src/main/java/com/kurtulussahin/codekata/httpsservertutorial/HttpServer.java
+++ src/main/java/com/kurtulussahin/codekata/httpsservertutorial/HttpServer.java
@@ -1,9 +1,17 @@
 package com.kurtulussahin.codekata.httpsservertutorial;
 
+import com.kurtulussahin.codekata.httpsservertutorial.config.Configuration;
+import com.kurtulussahin.codekata.httpsservertutorial.config.ConfigurationManager;
+
 public class HttpServer {
 
     public static void main(String[] args){
 
         System.out.println("Server starting ...");
+
+        ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
+        Configuration conf= ConfigurationManager.getInstance().getCurrentConfigurtion();
+        System.out.println("Using Port: "+conf.getPort());
+        System.out.println("Using WebRoot: "+conf.getWebroot());
     }
 }
