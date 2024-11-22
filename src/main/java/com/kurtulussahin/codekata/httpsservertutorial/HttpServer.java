35da00b7b3872a3a6c884cb7f9024091b9013eef
kurtulussahin <kurtulussahin@gmail.com>
2024-11-22 19:00:36 +0300

    first commit of simple http server project

--- src/main/java/com/kurtulussahin/codekata/httpsservertutorial/HttpServer.java
+++ src/main/java/com/kurtulussahin/codekata/httpsservertutorial/HttpServer.java
@@ -3,6 +3,7 @@ package com.kurtulussahin.codekata.httpsservertutorial;
 public class HttpServer {
 
     public static void main(String[] args){
+
         System.out.println("Server starting ...");
     }
 }
