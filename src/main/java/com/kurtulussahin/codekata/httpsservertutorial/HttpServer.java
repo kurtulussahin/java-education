49484522420cbb749a926ae150106ef65e4bac13
kurtulussahin <kurtulussahin@gmail.com>
2024-11-22 18:59:49 +0300

    directory fix

---
+++ src/main/java/com/kurtulussahin/codekata/httpsservertutorial/HttpServer.java
@@ -0,0 +1,8 @@
+package com.kurtulussahin.codekata.httpsservertutorial;
+
+public class HttpServer {
+
+    public static void main(String[] args){
+        System.out.println("Server starting ...");
+    }
+}
