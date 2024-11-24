f22a74f7324c360ba48c37503b930751612e94a8
kurtulussahin <kurtulussahin@gmail.com>
2024-11-24 18:36:21 +0300

    unused imports removed

--- src/main/java/com/kurtulussahin/codekata/httpsservertutorial/HttpServer.java
+++ src/main/java/com/kurtulussahin/codekata/httpsservertutorial/HttpServer.java
@@ -7,10 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 public class HttpServer {
 
--- src/main/java/com/kurtulussahin/codekata/httpsservertutorial/core/ServerListenerThread.java
+++ src/main/java/com/kurtulussahin/codekata/httpsservertutorial/core/ServerListenerThread.java
@@ -1,12 +1,9 @@
 package com.kurtulussahin.codekata.httpsservertutorial.core;
 
-import com.kurtulussahin.codekata.httpsservertutorial.HttpServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -33,37 +30,19 @@ public class ServerListenerThread extends Thread{
 
                 LOGGER.info(" * Connection accepted " + socket.getInetAddress());
 
-                InputStream inputStream = socket.getInputStream();
-                OutputStream outputStream = socket.getOutputStream();
-
-                String html = "<html><head><title>Simple Java HTTP Server</title></head>" +
-                        "<body><h1>This page served using simple http server</h1></body></html>";
-
-                final String CRLF = "\n\r"; // 13,10 ASCII
-
-                String response =
-                        "HTTP/1.1 200 OK" + CRLF +  //Status line : HTTP_VERSION RESPONSE_CODE RESPONSE_MESSAGE
-                                "Content-Length" + html.getBytes().length + CRLF + // HEADER
-                                CRLF +
-                                html +
-                                CRLF + CRLF;
-
-                outputStream.write(response.getBytes());
-
-                inputStream.close();
-                outputStream.close();
-                socket.close();
-
-                try {
-                    sleep(9000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
+                HttpConnectionWorkerThread workerThread = new HttpConnectionWorkerThread(socket);
+                workerThread.start();
             }
             // serverSocket.close(); //TODO
 
         } catch (IOException e) {
-            e.printStackTrace();
+            LOGGER.error("Problem with setting socket", e);
+        }finally {
+            if(serverSocket!=null){
+                try {
+                    serverSocket.close();
+                } catch (IOException e) {}
+            }
         }
 
     }
