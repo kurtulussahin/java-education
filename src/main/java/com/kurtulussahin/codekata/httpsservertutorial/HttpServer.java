66e248f8ef8f6e6da25fa60a71fecc29c0c7d30a
kurtulussahin <kurtulussahin@gmail.com>
2024-11-24 17:59:19 +0300

    single thread accept

--- src/main/java/com/kurtulussahin/codekata/httpsservertutorial/HttpServer.java
+++ src/main/java/com/kurtulussahin/codekata/httpsservertutorial/HttpServer.java
@@ -2,6 +2,9 @@ package com.kurtulussahin.codekata.httpsservertutorial;
 
 import com.kurtulussahin.codekata.httpsservertutorial.config.Configuration;
 import com.kurtulussahin.codekata.httpsservertutorial.config.ConfigurationManager;
+import com.kurtulussahin.codekata.httpsservertutorial.core.ServerListenerThread;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,41 +14,21 @@ import java.net.Socket;
 
 public class HttpServer {
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(HttpServer.class);
+
     public static void main(String[] args){
 
-        System.out.println("Server starting ...");
+        LOGGER.info("Server starting ...");
 
         ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
         Configuration conf= ConfigurationManager.getInstance().getCurrentConfigurtion();
-        System.out.println("Using Port: "+conf.getPort());
-        System.out.println("Using WebRoot: "+conf.getWebroot());
+
+        LOGGER.info("Using Port: "+conf.getPort());
+        LOGGER.info("Using WebRoot: "+conf.getWebroot());
 
         try {
-            ServerSocket serverSocket = new ServerSocket(conf.getPort());
-            Socket socket = serverSocket.accept();
-
-            InputStream inputStream = socket.getInputStream();
-            OutputStream outputStream = socket.getOutputStream();
-
-            String html = "<html><head><title>Simple Java HTTP Server</title></head>" +
-                    "<body><h1>This page served using simple http server</h1></body></html>";
-
-            final String CRLF="\n\r"; // 13,10 ASCII
-
-            String response =
-                    "HTTP/1.1 200 OK" + CRLF +  //Status line : HTTP_VERSION RESPONSE_CODE RESPONSE_MESSAGE
-                    "Content-Length" + html.getBytes().length + CRLF + // HEADER
-                            CRLF +
-                            html +
-                            CRLF + CRLF;
-
-            outputStream.write(response.getBytes());
-
-            inputStream.close();
-            outputStream.close();
-            socket.close();
-            serverSocket.close();
-
+            ServerListenerThread serverListenerThread = new ServerListenerThread(conf.getPort(),conf.getWebroot());
+            serverListenerThread.start();
         } catch (IOException e) {
             e.printStackTrace();
         }
