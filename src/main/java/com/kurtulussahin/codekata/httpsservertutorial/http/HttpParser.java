ef10dbf71ef4337591836d49e1ef572161f832dc
kurtulussahin <kurtulussahin@gmail.com>
2024-11-26 21:17:38 +0300

    request line parsing, test cases, exceptions complated

--- src/main/java/com/kurtulussahin/codekata/httpsservertutorial/http/HttpParser.java
+++ src/main/java/com/kurtulussahin/codekata/httpsservertutorial/http/HttpParser.java
@@ -52,6 +52,13 @@ public class HttpParser {
                     if(!methodParsed || !requestTargetParsed){
                         throw new HttpParsingException(HttpStatusCode.CLIENT_ERROR_400_BAD_REQUEST);
                     }
+
+                    try {
+                        request.setHttpVersion(processingDataBuffer.toString());
+                    } catch (BadHttpVersionException e) {
+                        throw new HttpParsingException(HttpStatusCode.CLIENT_ERROR_400_BAD_REQUEST);
+                    }
+
                     return;
                 } else {
                     throw new HttpParsingException(HttpStatusCode.CLIENT_ERROR_400_BAD_REQUEST);
@@ -66,6 +73,7 @@ public class HttpParser {
                     methodParsed=true;
                 } else if(!requestTargetParsed){
                     LOGGER.debug("Request Line REQ TARGET to Process : {}", processingDataBuffer.toString());
+                    request.setRequestTarget(processingDataBuffer.toString());
                     requestTargetParsed=true;
                 } else{
                     throw new HttpParsingException(HttpStatusCode.CLIENT_ERROR_400_BAD_REQUEST);
--- src/main/java/com/kurtulussahin/codekata/httpsservertutorial/test/HttpParserTest.java
+++ src/main/java/com/kurtulussahin/codekata/httpsservertutorial/test/HttpParserTest.java
@@ -1,5 +1,7 @@
-package com.kurtulussahin.codekata.httpsservertutorial.http;
+package com.kurtulussahin.codekata.httpsservertutorial.test;
 
+import com.kurtulussahin.codekata.httpsservertutorial.http.*;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -8,8 +10,7 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class HttpParserTest {
@@ -30,7 +31,11 @@ class HttpParserTest {
             fail(e);
         }
 
+        assertNotNull(request);
         assertEquals(HttpMethod.GET,request.getMethod());
+        assertEquals("/", request.getRequestTarget());
+        assertEquals("HTTP/1.1", request.getOriginalHttpVersion());
+        assertEquals(HttpVersion.HTTP_1_1, request.getBestCompatibleHttpVersion());
     }
 
     @Test
@@ -39,7 +44,7 @@ class HttpParserTest {
             HttpRequest request = httpParser.parseHttpRequest(generateBadTestCaseMethodName1());
             fail();
         } catch (HttpParsingException e) {
-            assertEquals(HttpStatusCode.SERVER_ERROR_501_NOT_IMPLEMENTED, e.getErrorCode());
+            Assertions.assertEquals(HttpStatusCode.SERVER_ERROR_501_NOT_IMPLEMENTED, e.getErrorCode());
         }
     }
 
@@ -80,6 +85,39 @@ class HttpParserTest {
             fail();
         } catch (HttpParsingException e) {
             assertEquals(HttpStatusCode.CLIENT_ERROR_400_BAD_REQUEST, e.getErrorCode());
+        }
+    }
+
+    @Test
+    void parseHttpRequestBadHttpVersion() {
+        try {
+            HttpRequest request = httpParser.parseHttpRequest(generateBadHttpVersionTestCase());
+            fail();
+        } catch (HttpParsingException e) {
+            assertEquals(HttpStatusCode.CLIENT_ERROR_400_BAD_REQUEST, e.getErrorCode());
+        }
+    }
+
+    @Test
+    void parseHttpRequestUnssportedHttpVersion() {
+        try {
+            HttpRequest request = httpParser.parseHttpRequest(generateUnsportedHttpVersionTestCase());
+            fail();
+        } catch (HttpParsingException e) {
+            assertEquals(HttpStatusCode.SERVER_ERROR_505_HTTP_VERSION_NOT_SUPPORTED, e.getErrorCode());
+        }
+    }
+
+    @Test
+    void parseHttpRequestSportedHttpVersion() {
+        try {
+            HttpRequest request = httpParser.parseHttpRequest(generateSportedHttpVersionTestCase());
+            assertNotNull(request);
+            assertEquals(HttpVersion.HTTP_1_1, request.getBestCompatibleHttpVersion());
+            assertEquals("HTTP/1.2",request.getOriginalHttpVersion());
+
+        } catch (HttpParsingException e) {
+            fail();
         }
     }
 
@@ -145,6 +183,60 @@ class HttpParserTest {
     private InputStream generateBadTestCaseRequestLineOnlyCRnoLF(){
         String rawData = "GET / HTTP/1.1\r" + //CR no LF
                 "Host: localhost:8080\r\n" +
+                "Accept-Language: en-US,en;q=0.9,es;q=0.8,pt;q=0.7,de-DE;q=0.6,de;q=0.5,la;q=0.4\r\n" +
+                "\r\n";
+
+        InputStream inputStream = new ByteArrayInputStream( rawData.getBytes(StandardCharsets.US_ASCII));
+        return inputStream;
+    }
+
+    private InputStream generateBadHttpVersionTestCase(){
+        String rawData = "GET / HTP/1.1\r\n" +
+                "Host: localhost:8080\r\n" +
+                "Connection: keep-alive\r\n" +
+                "Upgrade-Insecure-Requests: 1\r\n" +
+                "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36\r\n" +
+                "Sec-Fetch-User: ?1\r\n" +
+                "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3\r\n" +
+                "Sec-Fetch-Site: none\r\n" +
+                "Sec-Fetch-Mode: navigate\r\n" +
+                "Accept-Encoding: gzip, deflate, br\r\n" +
+                "Accept-Language: en-US,en;q=0.9,es;q=0.8,pt;q=0.7,de-DE;q=0.6,de;q=0.5,la;q=0.4\r\n" +
+                "\r\n";
+
+        InputStream inputStream = new ByteArrayInputStream( rawData.getBytes(StandardCharsets.US_ASCII));
+        return inputStream;
+    }
+
+    private InputStream generateUnsportedHttpVersionTestCase(){
+        String rawData = "GET / HTTP/2.1\r\n" +
+                "Host: localhost:8080\r\n" +
+                "Connection: keep-alive\r\n" +
+                "Upgrade-Insecure-Requests: 1\r\n" +
+                "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36\r\n" +
+                "Sec-Fetch-User: ?1\r\n" +
+                "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3\r\n" +
+                "Sec-Fetch-Site: none\r\n" +
+                "Sec-Fetch-Mode: navigate\r\n" +
+                "Accept-Encoding: gzip, deflate, br\r\n" +
+                "Accept-Language: en-US,en;q=0.9,es;q=0.8,pt;q=0.7,de-DE;q=0.6,de;q=0.5,la;q=0.4\r\n" +
+                "\r\n";
+
+        InputStream inputStream = new ByteArrayInputStream( rawData.getBytes(StandardCharsets.US_ASCII));
+        return inputStream;
+    }
+
+    private InputStream generateSportedHttpVersionTestCase(){
+        String rawData = "GET / HTTP/1.2\r\n" +
+                "Host: localhost:8080\r\n" +
+                "Connection: keep-alive\r\n" +
+                "Upgrade-Insecure-Requests: 1\r\n" +
+                "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36\r\n" +
+                "Sec-Fetch-User: ?1\r\n" +
+                "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3\r\n" +
+                "Sec-Fetch-Site: none\r\n" +
+                "Sec-Fetch-Mode: navigate\r\n" +
+                "Accept-Encoding: gzip, deflate, br\r\n" +
                 "Accept-Language: en-US,en;q=0.9,es;q=0.8,pt;q=0.7,de-DE;q=0.6,de;q=0.5,la;q=0.4\r\n" +
                 "\r\n";
 
