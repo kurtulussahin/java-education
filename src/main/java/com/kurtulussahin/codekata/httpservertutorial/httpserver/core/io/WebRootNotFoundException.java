cf248769a5de042d3c3d979f4cb0039446e64600
kurtulussahin <kurtulussahin@gmail.com>
2024-11-29 21:44:36 +0300

    webrootHendler, tests, exceptions

---
+++ src/main/java/com/kurtulussahin/codekata/httpservertutorial/httpserver/core/io/WebRootNotFoundException.java
@@ -0,0 +1,7 @@
+package com.kurtulussahin.codekata.httpservertutorial.httpserver.core.io;
+
+public class WebRootNotFoundException extends Throwable {
+    public WebRootNotFoundException(String message) {
+        super(message);
+    }
+}
