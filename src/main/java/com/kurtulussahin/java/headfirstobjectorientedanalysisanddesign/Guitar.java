769304b75920d2759f870f7559625ed8902dec7e
kurtulussahin <kurtulussahin@gmail.com>
2025-02-02 18:07:28 +0300

    new GuitarSpec Class encapsulated

--- src/main/java/com/kurtulussahin/java/headfirstobjectorientedanalysisanddesign/Guitar.java
+++ src/main/java/com/kurtulussahin/java/headfirstobjectorientedanalysisanddesign/Guitar.java
@@ -3,21 +3,13 @@ package com.kurtulussahin.java.headfirstobjectorientedanalysisanddesign;
 public class Guitar {
 
     private String serialNumber;
-    private Builder builder;
-    private String model;
-    private Type type;
-    private Wood backWood;
-    private Wood topWood;
     private double price;
+    private GuitarSpec spec;
 
     public Guitar(String serialNumber, double price, Builder builder, String model, Type type, Wood backWood, Wood topWood) {
         this.serialNumber = serialNumber;
         this.price = price;
-        this.builder = builder;
-        this.model = model;
-        this.type = type;
-        this.backWood = backWood;
-        this.topWood = topWood;
+        this.spec=new GuitarSpec(builder,model,type,backWood,topWood);
     }
 
     public String getSerialNumber() {
@@ -30,19 +22,7 @@ public class Guitar {
     public void setPrice(float newPrice) {
         this.price = newPrice;
     }
-    public Builder getBuilder() {
-        return builder;
-    }
-    public String getModel() {
-        return model;
-    }
-    public Type getType() {
-        return type;
-    }
-    public Wood getBackWood() {
-        return backWood;
-    }
-    public Wood getTopWood() {
-        return topWood;
+    public GuitarSpec getSpec() {
+        return spec;
     }
 }
